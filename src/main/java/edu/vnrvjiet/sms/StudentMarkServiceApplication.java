a70bccc10556717468b08e3471caf5a849a105cb
srimanikantaBattu <srimanikantabattu04@gmail.com>
2025-11-25 10:29:34 +0530

    Added to github

---
+++ src/main/java/edu/vnrvjiet/sms/StudentMarkServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.vnrvjiet.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMarkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMarkServiceApplication.class, args);
+        System.out.println("Tomcat running for Student Mark Microservice... Testing");
+	}
+}
